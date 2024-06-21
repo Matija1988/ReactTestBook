8812c04fd40ff3d939d16e1581bbbf512fa69ede
Matija1988 <matijapavkovic74@gmail.com>
2024-06-21 15:00:38 +0200

    Delete and create with id

--- Pages/Author/AuthorCRUD.jsx
+++ Pages/Author/AuthorCRUD.jsx
@@ -20,21 +20,31 @@ export default function Author() {
     }, []);
 
     function handleUpdate(author) {
-        alert("Update presed " + JSON.stringify(author));
+        alert("Update pressed " + JSON.stringify(author));
     }
 
     function handleDelete(author) {
-        alert("Delete presed " + JSON.stringify(author));
+        alert("Delete pressed " + JSON.stringify(author));
+
+        let idToRemove = author.Id;
+
+       
+        setAuthors(authors.filter(item => item.Id !== author.Id));
     }
 
-    function getLastId(authorSeed) {    
-           
+    function getLastId(authors) {    
+           const getLastAuthor = authors[authors.length - 1];
+           const last = getLastAuthor.Id;
+        
+           return last;
+      
     }
 
     function handleCreate(author) {
-        const lastId = getLastId(authorSeed);
+        const lastId = getLastId(authors);
         const id = lastId + 1;
         const newAuthor = {id, ...author};
+        newAuthor.Id = id;
         setAuthors([...authors, newAuthor]);
        
     } 
